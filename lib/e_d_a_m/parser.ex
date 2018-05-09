9dbfad65a5d1706a0072b65da4f2263f8e24fe8c
Ivan Kuzmin <inkuzm@gmail.com>
2018-05-09 15:36:50 +0300

    reconsider data structure

--- lib/e_d_a_m/parser.ex
+++ lib/e_d_a_m/parser.ex
@@ -49,45 +49,50 @@ defmodule EDAM.Parser do
     hierarchy = build_hierarchy("http://www.w3.org/2002/07/owl#Thing", relations, %{})
 
     #    h |> Map.get("http://www.w3.org/2002/07/owl#Thing") |> Enum.at(0) # deprecated
+    #    IO.puts(hierarchy)
 
-    [topic_root] = hierarchy
-                   |> Map.get("http://www.w3.org/2002/07/owl#Thing")
-                   |> Enum.at(1)
-                   |> Map.keys
+    topic_root = hierarchy
+                 |> Map.get(:nodes)
+                 |> Enum.at(1)
+                 |> Map.get(:label)
+
     topic = hierarchy
-            |> Map.get("http://www.w3.org/2002/07/owl#Thing")
+            |> Map.get(:nodes)
             |> Enum.at(1)
             |> trim_to_ids(topic_root, %{}, &extract_topic_id/1)
 
 
-    [operation_root] = hierarchy
-                       |> Map.get("http://www.w3.org/2002/07/owl#Thing")
-                       |> Enum.at(2)
-                       |> Map.keys
+    operation_root = hierarchy
+                 |> Map.get(:nodes)
+                 |> Enum.at(2)
+                 |> Map.get(:label)
+
     operation = hierarchy
-                |> Map.get("http://www.w3.org/2002/07/owl#Thing")
-                |> Enum.at(2)
-                |> trim_to_ids(operation_root, %{}, &extract_operation_id/1)
+            |> Map.get(:nodes)
+            |> Enum.at(2)
+            |> trim_to_ids(operation_root, %{}, &extract_operation_id/1)
 
 
-    [format_root] = hierarchy
-                    |> Map.get("http://www.w3.org/2002/07/owl#Thing")
-                    |> Enum.at(3)
-                    |> Map.keys
+    format_root = hierarchy
+                 |> Map.get(:nodes)
+                 |> Enum.at(3)
+                 |> Map.get(:label)
+
     format = hierarchy
-             |> Map.get("http://www.w3.org/2002/07/owl#Thing")
-             |> Enum.at(3)
-             |> trim_to_ids(format_root, %{}, &extract_format_id/1)
+            |> Map.get(:nodes)
+            |> Enum.at(3)
+            |> trim_to_ids(format_root, %{}, &extract_format_id/1)
 
-    [data_root] = hierarchy
-                  |> Map.get("http://www.w3.org/2002/07/owl#Thing")
-                  |> Enum.at(4)
-                  |> Map.keys
+
+    data_root = hierarchy
+                 |> Map.get(:nodes)
+                 |> Enum.at(4)
+                 |> Map.get(:label)
+
     data = hierarchy
-           |> Map.get("http://www.w3.org/2002/07/owl#Thing")
-           |> Enum.at(4)
-           |> trim_to_ids(data_root, %{}, &extract_data_id/1)
-
+            |> Map.get(:nodes)
+            |> Enum.at(4)
+            |> trim_to_ids(data_root, %{}, &extract_data_id/1)
 
     h = %{
       :topic => topic,
@@ -106,57 +111,72 @@ defmodule EDAM.Parser do
 
     File.write(
       "edam-search-data.json",
-      Enum.filter(descriptions, fn({k, v}) -> Regex.match?(~r{http://edamontology.org/data_}, k) end) |> to_hash
+      Enum.filter(descriptions, fn ({k, v}) -> Regex.match?(~r{http://edamontology.org/data_}, k) end)
+      |> to_hash(&extract_data_id/1)
       |> Poison.encode!(pretty: pretty),
       [:binary]
     )
 
     File.write(
       "edam-search-operation.json",
-      Enum.filter(descriptions, fn({k, v}) -> Regex.match?(~r{http://edamontology.org/operation_}, k) end) |> to_hash
+      Enum.filter(descriptions, fn ({k, v}) -> Regex.match?(~r{http://edamontology.org/operation_}, k) end)
+      |> to_hash(&extract_operation_id/1)
       |> Poison.encode!(pretty: pretty),
       [:binary]
     )
 
     File.write(
       "edam-search-topic.json",
-      Enum.filter(descriptions, fn({k, v}) -> Regex.match?(~r{http://edamontology.org/topic_}, k) end) |> to_hash
+      Enum.filter(descriptions, fn ({k, v}) -> Regex.match?(~r{http://edamontology.org/topic_}, k) end)
+      |> to_hash(&extract_topic_id/1)
       |> Poison.encode!(pretty: pretty),
       [:binary]
     )
 
     File.write(
       "edam-search-format.json",
-      Enum.filter(descriptions, fn({k, v}) -> Regex.match?(~r{http://edamontology.org/format_}, k) end) |> to_hash
+      Enum.filter(descriptions, fn ({k, v}) -> Regex.match?(~r{http://edamontology.org/format_}, k) end)
+      |> to_hash(&extract_format_id/1)
       |> Poison.encode!(pretty: pretty),
       [:binary]
     )
   end
 
 
-  def to_hash(struct) do
-    Enum.map(struct, fn({k, v}) ->
-      %{k => v}
-    end)
+  def to_hash(struct, fun) do
+    Enum.map(
+      struct,
+      fn ({k, v}) ->
+        %{
+          :id => fun.(k),
+          :label => Map.get(v, :label),
+          :description => Map.get(v, :description),
+          :synonyms => Map.get(v, :synonyms)
+        }
+      end
+    )
   end
 
   def trim_to_ids(tree, node, acc, fun) do
-    Map.put(
-      acc,
-      fun.(node),
-      case Map.get(tree, node) do
-        nil -> nil
-        children ->
+    n = Map.put(acc, :label, fun.(node))
+
+    case Map.get(tree, :nodes) do
+      nil -> nil
+      children ->
+        n = Map.put(
+          n,
+          :nodes,
           Enum.map(
             children,
             fn (child) ->
-              [root] = child
-                       |> Map.keys
+              root = child
+                     |> Map.get(:label)
               trim_to_ids(child, root, %{}, fun)
             end
           )
-      end
-    )
+        )
+    end
+    n
   end
 
   def extract_topic_id(nil) do
@@ -206,20 +226,24 @@ defmodule EDAM.Parser do
 
   def build_hierarchy(parent, relations, acc) do
     empty_map = %{}
-    Map.put(
-      acc,
-      parent,
-      case Map.get(relations, parent) do
-        nil -> nil
-        children ->
+    node = Map.put(acc, :label, parent)
+
+    case Map.get(relations, parent) do
+      nil -> nil
+      children ->
+        node = Map.put(
+          node,
+          :nodes,
           Enum.map(
             children,
             fn (child) ->
               build_hierarchy(child, relations, %{})
             end
           )
-      end
-    )
+        )
+    end
+
+    node
   end
 
   def build_tree(parent, relations, descriptions, acc) do
